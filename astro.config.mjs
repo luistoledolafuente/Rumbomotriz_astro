b99e194ba0ce7076ce732b4f2bb8e46e4726fcba
luistoledolafuente <luismiguel.tlf@gmail.com>
2026-07-29 17:47:21 -0500

    feat: Migrar assets a Cloudflare R2 via cdn.rumbomotriz.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://rumbomotriz.cl',
+  site: 'https://rumbomotriz.com',
   vite: {
     plugins: [tailwindcss()]
   },
